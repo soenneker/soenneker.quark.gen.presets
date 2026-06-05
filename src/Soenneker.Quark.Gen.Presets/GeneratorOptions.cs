7cdebd6821d1f9c1718e95fb508738bfadf036a4
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:37:17 -0500

    xml commenting

--- src/Soenneker.Quark.Gen.Presets/GeneratorOptions.cs
+++ src/Soenneker.Quark.Gen.Presets/GeneratorOptions.cs
@@ -5,7 +5,13 @@ namespace Soenneker.Quark.Gen.Presets;
 /// </summary>
 public sealed class GeneratorOptions
 {
+    /// <summary>
+    /// The default.
+    /// </summary>
     public static readonly GeneratorOptions Default = new();
 
+    /// <summary>
+    /// Gets or sets a value indicating whether emit diagnostics.
+    /// </summary>
     public bool EmitDiagnostics { get; set; }
 }
